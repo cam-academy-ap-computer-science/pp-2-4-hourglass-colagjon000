630499f2fb6a1da0bc17373f503ef3ea25ad8760
Jonah Colagross <colagjon000@battlegroundps.org>
2019-10-10 07:25:29 -0700

    Added topHalf and bottomHalf. Added more commenting.

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -12,19 +12,21 @@
 		-Increase colons per level of height
 - Loop in order to make sure center neck portion of hourglass is directly in the middle using spaces based on constant
 
--NOTES -- Used integer "counter" to make numerical relation more obvious and easily changeable
+-NOTES -- Used integer "counter" etc. to make numerical relation more obvious and easily changeable/understandable
 */
 
 public class Hourglass {
 	/************************************************************************/
 	/*CLASS CONSTANT TO ADJUST HOURGLASS SIZE:                              */
 	/*(Size determines the height of the triangular portion of the hourglass*/
-	/*(Default Size  = 4)                                                   */
+	/*(Default Size  = 4) (Integer must be >0)                              */
 	public static final int SIZE = 4;
 	/************************************************************************/
 	public static void main (String[] args) {
 		bases();
+		topHalf();
 		centerNeck();
+		bottomHalf();
 		bases();
 	}
 	public static void bases () { //creates the top and bottom of the hourglass
@@ -43,6 +45,33 @@ public class Hourglass {
 		System.out.println("||");
 	}
 	public static void topHalf () { //creates top half of hourglass body
-		
+		int counterC = SIZE * 2; //colon count is always size * 2 to start
+		for (int i = 1; i <= SIZE; i++) { //creates layers of hourglass -- terminates when desired defined level reached
+			for (int j = 1; j <= i; j++) { //adds spaces for each layer of hourglass -- spaces = layer # (i)
+				System.out.print(" ");
+			}
+			System.out.print("\\");
+			for (int k = 1; k <= counterC; k++) {
+				System.out.print(":"); //prints the colon 2 * the SIZE times
+			}
+			System.out.println("/");
+			counterC -=2; //overwrite colon count to add another step
+		}
+	}
+	public static void bottomHalf () { //creates bottom half of hourglass body
+		int counterC = 2;
+		int spaceCount = SIZE;
+		for (int i = 1; i <= SIZE; i++) { //creates layers of hourglass -- terminates when desired defined level reached
+			for (int j = spaceCount; j >= 1; j--) { //removes spaces for each layer of hourglass -- starting spaces = layer # (i)
+				System.out.print(" ");
+			}
+			System.out.print("/");
+			for (int k = 1; k <= counterC; k++) {
+				System.out.print(":"); //prints the colon 2, then adding 2 until reaching desired level of size
+			}
+			System.out.println("\\");
+			counterC+=2;
+			spaceCount--;
+		}
 	}
 }
